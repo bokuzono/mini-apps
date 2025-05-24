0952658567f7d84317c02e312df3ac9d365afbde
久保菌祐太 <bokuzono@MacBook--M2.local>
2025-05-24 13:27:19 +0900

    ボタンと画像のレイアウトを更新

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,13 +1,15 @@
 import { memo } from "react";
+import { Link } from "react-router-dom";
 
 export const Header = memo(() => {
+
     return (
         <>
-            <header className="h-32 w-full flex">
-                <h1 className="text-lg">ミニアプリ集</h1>
-                <div className="flex">
-                    <p className="text-sm">ランダム画像</p>
-                    <p className="text-sm">ToDoリスト</p>
+            <header className="px-4 h-16 w-full flex items-center bg-[#8FA4B2]">
+                <h1 className="text-xl text-white font-bold mr-9">ミニアプリ集</h1>
+                <div className="flex gap-4">
+                    <Link to="/RandomPhoto" className="text-base text-white hover:text-opacity-60 hover:text-opacity-60">ランダム画像</Link>
+                    <Link to="/Todo" className="text-base text-white hover:text-opacity-60">ToDoリスト</Link>
                 </div>
             </header>
         </>
--- src/components/pages/RandomPhoto.tsx
+++ src/components/pages/RandomPhoto.tsx
@@ -1,5 +1,14 @@
 import { memo } from "react";
 
 export const RandomPhoto = memo(() => {
-    return <p>RandomPhoto</p>
+    return (
+        <div className="mx-auto py-8">
+            <div className="mb-8 flex justify-center">
+                <button className="bg-[#8FB29D] py-1 px-4 rounded-full text-white hover:translate-y-1 hover:scale-95 transition-transform duration-150">Changeで</button>
+            </div>
+            <div className="flex justify-center">
+                <img className="w-[500px]" src="https://picsum.photos/800" alt="ランダム画像" />
+            </div>
+        </div>
+    )
 });
